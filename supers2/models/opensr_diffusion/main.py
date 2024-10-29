5d58586d4aad2fc4ca4600864dcca68dabf7996d
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2024-10-29 11:56:17 +0100

    up

--- supers2/models/opensr_diffusion/main.py
+++ supers2/models/opensr_diffusion/main.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 from opensr_model.utils import linear_transform_4b
 from tqdm import tqdm
+from skimage.exposure import match_histograms
 
 from supers2.models.opensr_diffusion.diffusion.latentdiffusion import \
     LatentDiffusion
@@ -93,6 +94,10 @@ class SRmodel(torch.nn.Module):
         X_dec = self.model.decode_first_stage(X_enc)
         X_dec = self.linear_transform(X_dec, stage="denorm")
 
+        # Apply spectral correction
+        for i in range(X_dec.shape[1]):
+            X_dec[:, i] = self.hq_histogram_matching(X_dec[:, i], self._X[:, i])
+
         # If the value is negative, set it to 0
         X_dec[X_dec < 0] = 0
 
@@ -179,3 +184,33 @@ class SRmodel(torch.nn.Module):
         sr = self._tensor_decode(latent)
         sr = revert_padding(sr, padding)
         return sr
+
+    def hq_histogram_matching(
+        self, image1: torch.Tensor, image2: torch.Tensor
+    ) -> torch.Tensor:
+        """Lazy implementation of histogram matching
+
+        Args:
+            image1 (torch.Tensor): The low-resolution image (C, H, W).
+            image2 (torch.Tensor): The super-resolved image (C, H, W).
+
+        Returns:
+            torch.Tensor: The super-resolved image with the histogram of
+                the target image.
+        """
+
+        # Go to numpy
+        np_image1 = image1.detach().cpu().numpy()
+        np_image2 = image2.detach().cpu().numpy()
+
+        if np_image1.ndim == 3:
+            np_image1_hat = match_histograms(np_image1, np_image2, channel_axis=0)
+        elif np_image1.ndim == 2:
+            np_image1_hat = match_histograms(np_image1, np_image2, channel_axis=None)
+        else:
+            raise ValueError("The input image must have 2 or 3 dimensions.")
+
+        # Go back to torch
+        image1_hat = torch.from_numpy(np_image1_hat).to(image1.device)
+
+        return image1_hat
